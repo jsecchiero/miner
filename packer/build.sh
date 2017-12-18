56c071fe9801f14c185af496ebb65bd9d4dc1f7e
jsecchiero <secchierojacopo@gmail.com>
2017-12-18 18:02:59 +0100

    update ci

--- packer/build.sh
+++ packer/build.sh
@@ -12,9 +12,9 @@ echo sudo lsmod | grep kvm
 sudo lsmod | grep kvm
 
 # Install requirements
-curl -L -o packer.zip https://releases.hashicorp.com/packer/1.1.3/packer_1.1.3_linux_amd64.zip && unzip -d bin packer.zip
+curl -L -o packer.zip https://releases.hashicorp.com/packer/1.1.3/packer_1.1.3_linux_amd64.zip && sudo unzip -o -d /usr/local/sbin/ packer.zip
 curl -L -o vagrant.deb https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb
-sudo apt-get install -f $(pwd)/vagrant.deb
+sudo apt-get install -y -f $(pwd)/vagrant.deb
 sudo apt-get install -y qemu libvirt-bin ebtables dnsmasq libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev
 
 cd $(dirname $0)
@@ -22,4 +22,4 @@ pwd
 # Download base image
 vagrant box add generic/ubuntu1604 --provider=libvirt --box-version=1.3.25 || true
 # Start build
-packer build -var "mirror=${HOME}/.vagrant.d/boxes/generic-VAGRANTSLASH-ubuntu1604/1.3.25/libvirt/box.img" -var "cloud_token=${CLOUD_TOKEN}" miner-amd.json
+sudo packer build -var "mirror=${HOME}/.vagrant.d/boxes/generic-VAGRANTSLASH-ubuntu1604/1.3.25/libvirt/box.img" -var "cloud_token=${CLOUD_TOKEN}" miner-amd.json
