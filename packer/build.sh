16c1aee24fb6a7e7e2481f16e524852018a75060
jsecchiero <secchierojacopo@gmail.com>
2017-12-18 19:30:20 +0100

    avoid build fail if version already exist

--- packer/build.sh
+++ packer/build.sh
@@ -22,4 +22,4 @@ pwd
 # Download base image
 vagrant box add generic/ubuntu1604 --provider=libvirt --box-version=1.3.25 || true
 # Start build
-sudo packer build -var "mirror=${HOME}/.vagrant.d/boxes/generic-VAGRANTSLASH-ubuntu1604/1.3.25/libvirt/box.img" -var "cloud_token=${CLOUD_TOKEN}" miner-amd.json
+sudo packer build -var "mirror=${HOME}/.vagrant.d/boxes/generic-VAGRANTSLASH-ubuntu1604/1.3.25/libvirt/box.img" -var "cloud_token=${CLOUD_TOKEN}" miner-amd.json || true
